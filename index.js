98afdaf1db9d7354b374d97d0fd31fa9a422d5e4
webfaisalbd <web.faisal.bd@gmail.com>
2022-04-24 17:23:16 +0600

    create dynamic api, api parameter, access params

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -6,6 +7,30 @@ app.get('/', (req,res)=>{
     res.send('This is my smarty node application');
 })
 
+const users = [
+    {id: 0, name: 'Faisal', email: 'faisal@gmail.com', phone: '01611111'},
+    {id: 1, name: 'Faisal', email: 'faisal@gmail.com', phone: '01711111'},
+    {id: 2, name: 'Ahmed', email: 'ahmed@gmail.com', phone: '017222222'},
+    {id: 3, name: 'Shajib', email: 'shajib@gmail.com', phone: '017333333'},
+    {id: 4, name: 'Mawa', email: 'mawa@gmail.com', phone: '017444444'},
+    {id: 5, name: 'Hawa', email: 'hawa@gmail.com', phone: '01755555'},
+    {id: 6, name: 'Isq', email: 'isq@gmail.com', phone: '017666666'},
+    {id: 7, name: 'Iqra', email: 'iqra@gmail.com', phone: '017777777'},
+]
+
+app.get('/users',(req,res)=>{
+    res.send(users);
+})
+
+app.get('/user/:id',(req,res) => {
+    console.log(req.params);
+    // const id=req.params.id;
+    const id= parseInt(req.params.id);
+    // const user=users[id];
+    const user = users.find(u => u.id === id)
+    res.send(user);
+})
+
 app.listen(port,()=>{
-    console.log("Started the node project");
+    console.log("Server running successfully");
 })
